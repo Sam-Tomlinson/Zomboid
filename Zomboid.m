bf4fa3ca85ac30dbcfc7ef02eb16659526be92fa
ronis-tobin1 <93795520+ronis-tobin1@users.noreply.github.com>
2021-11-05 16:34:38 -0400

    Update Zomboid.m

--- Zomboid.m
+++ Zomboid.m
@@ -5,3 +5,6 @@ while alive && time <= 500
 	fprintf('Its been two weeks since the zombie apocolypose started.')
     fprintf('I have been listening to the radio all day.')
     fprintf('My name is daniel and I am very attractive')
+for i = (1:5)
+    i == dick
+end
