895011308761292446b60c24f94e8fe4a1d86d51
Sam-Tomlinson <tomlinson.174@osu.edu>
2021-11-05 16:23:55 -0400

    Update Zomboid.m
    Started working on the game file

--- Zomboid.m
+++ Zomboid.m
@@ -1,2 +1,5 @@
-fprintf('Is this thing on?')
-fprintf('What about now?')
+%Zomboid: Escape from Columbus 
+alive = true;
+time = 0;
+while alive && time <= 500
+	fprintf('Its been two weeks since the zombie apocolypose started.')
