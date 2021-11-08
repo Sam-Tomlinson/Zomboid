1c5aca165e8f5107c26bb37779a698052f333177
Sam-Tomlinson <tomlinson.174@osu.edu>
2021-11-08 17:23:26 -0500

    No change to main game
    just doing some test scene drawing.

--- Zomboid.m
+++ Zomboid.m
@@ -1,6 +1,5 @@
 %Zomboid: Escape from Columbus 
-clc
-clear
+clc; clear; close all;
 zomboid = simpleGameEngine('retro_pack.png',16,16,5);
 drawScene(zomboid,[[529   530  327 530   531];[561     1  1   1   563];...
     [561     1  26   1   563];[561     1  1   1   563];...
