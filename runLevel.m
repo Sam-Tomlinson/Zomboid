ee84397820c700b0fc65c3a0f4630e11c913c389
Sam-Tomlinson <tomlinson.174@osu.edu>
2021-11-09 10:33:51 -0500

    Renamed unLayer to runLevel
    Changed unLayer function so that it will run the level too.

--- runLevel.m
+++ runLevel.m
@@ -1,5 +1,5 @@
-function [layer1,layer2] = unLayer(saveFile)
-    
+function runLevel(gameObject, saveFile)
+
     fullLevel = load(saveFile);
     fullDim = size(fullLevel);
     numOfRows = fullDim(1) ./ 2;
@@ -7,3 +7,7 @@ function [layer1,layer2] = unLayer(saveFile)
        layer1(i,:) = fullLevel(i,:);
        layer2(i,:) = fullLevel(i+numOfRows,:);
     end
+    
+    drawScene(gameObject,layer1,layer2)
+end
+
