2ec812f479289e698d2af05d582ab088a82ead36
Sam-Tomlinson <89880561+Sam-Tomlinson@users.noreply.github.com>
2021-11-28 17:32:56 -0500

    Started working on implementing the fprintf statements

--- waitgame.m
+++ waitgame.m
@@ -1,7 +1,7 @@
-function waitgame(type,timeWait)
+function waitgame(timeWait)
     % The game will pause for timeWait seconds and print: . . . 
     for i = 1:3
-        cprintf(type,'. ')
+        cprintf('. ')
         pause(timeWait/3)
     end 
 end
